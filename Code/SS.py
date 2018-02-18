384e097d6788e3c6c1a9da0bb7a02d695033af0b
jdebacker <jason.debacker@gmail.com>
2018-02-17 21:14:06 -0500

    surpress print statements for estimation

--- Code/SS.py
+++ Code/SS.py
@@ -47,45 +47,45 @@ def find_SD(PF_k, PF_b, Pi, sizez, sizek, sizeb, Gamma_initial):
         Gamma = HGamma
         SDiter += 1
 
-    if SDiter < SDmaxiter:
-        print('Stationary distribution converged after this many iterations: ',
-              SDiter)
-    else:
-        print('Stationary distribution did not converge')
-
-    # Check if state space is binding
-    if (Gamma.sum(axis=0)).sum(axis=1)[-1] > 0.002:
-        print('Stationary distribution is binding on k-grid.  Consider ' +
-              'increasing the upper bound.')
-    if (Gamma.sum(axis=0)).sum(axis=0)[-1] > 0.01:
-        print('Stationary distribution is binding on b-grid.  Consider ' +
-              'increasing the upper bound.')
-    if (Gamma.sum(axis=0)).sum(axis=0)[0] > 0.01:
-        print('Stationary distribution is binding on b-grid.  Consider ' +
-              'decreasing the lower bound.')
+    # if SDiter < SDmaxiter:
+    #     print('Stationary distribution converged after this many iterations: ',
+    #           SDiter)
+    # else:
+    #     print('Stationary distribution did not converge')
+    #
+    # # Check if state space is binding
+    # if (Gamma.sum(axis=0)).sum(axis=1)[-1] > 0.002:
+    #     print('Stationary distribution is binding on k-grid.  Consider ' +
+    #           'increasing the upper bound.')
+    # if (Gamma.sum(axis=0)).sum(axis=0)[-1] > 0.01:
+    #     print('Stationary distribution is binding on b-grid.  Consider ' +
+    #           'increasing the upper bound.')
+    # if (Gamma.sum(axis=0)).sum(axis=0)[0] > 0.01:
+    #     print('Stationary distribution is binding on b-grid.  Consider ' +
+    #           'decreasing the lower bound.')
 
     return Gamma
 
 
 def Market_Clearing(w, args):
-    (r, alpha_k, alpha_l, delta, psi, betafirm, kgrid, zgrid, bgrid, Pi,
-     eta0, eta1, eta2, s, sizek, sizez, sizeb, h, tax_params, VF_initial,
-     Gamma_initial) = args
+    (r, alpha_k, alpha_l, delta, psi, fixed_cost, betafirm, kgrid, zgrid,
+     bgrid, Pi, eta0, eta1, eta2, s, sizek, sizez, sizeb, h, tax_params,
+     VF_initial, Gamma_initial) = args
     # print('VF_initial = ', VF_initial[:4, 50:60])
     tau_l, tau_i, tau_d, tau_g, tau_c, f_e, f_b = tax_params
-    op, e, l_d, y, eta, collateral_constraint = VFI.get_firmobjects(r, w, zgrid, kgrid, bgrid,
-                                             alpha_k, alpha_l, delta,
-                                             psi, eta0, eta1, eta2, s,
-                                             sizez, sizek, sizeb, tax_params)
-    VF, PF_k, PF_b, optK, optI, optB = VFI.VFI(e, eta, collateral_constraint, betafirm, delta,
-                                               kgrid, bgrid, Pi, sizez, sizek,
-                                               sizeb, tax_params, VF_initial)
+    op, e, l_d, y, eta, collateral_constraint =\
+        VFI.get_firmobjects(r, w, zgrid, kgrid, bgrid, alpha_k, alpha_l,
+                            delta, psi, fixed_cost, eta0, eta1, eta2, s,
+                            sizez, sizek, sizeb, tax_params)
+    VF, PF_k, PF_b, optK, optI, optB =\
+        VFI.VFI(e, eta, collateral_constraint, betafirm, delta, kgrid,
+                bgrid, Pi, sizez, sizek, sizeb, tax_params, VF_initial)
     Gamma = find_SD(PF_k, PF_b, Pi, sizez, sizek, sizeb, Gamma_initial)
     L_d = (Gamma.sum(axis=2) * l_d).sum()
     Y = (Gamma.sum(axis=2) * y).sum()
     I = (Gamma * optI).sum()
     k3grid = np.tile(np.reshape(kgrid, (1, sizek, 1)), (sizez, 1, sizeb))
-    Psi = (Gamma * VFI.adj_costs(optK, k3grid, delta, psi)).sum()
+    Psi = (Gamma * VFI.adj_costs(optK, k3grid, delta, psi, fixed_cost)).sum()
     C = Y - I - Psi
     # note that financial frictions not here- they aren't real costs,
     # rather they are costs paid by firms and recieved for financial
@@ -107,9 +107,9 @@ def golden_ratio_eqm(lb, ub, args, tolerance=1e-4):
     '''
     Use the golden section search method to find the GE
     '''
-    (r, alpha_k, alpha_l, delta, psi, betafirm, kgrid, zgrid, bgrid, Pi,
-     eta0, eta1, eta2, s, sizek, sizez, sizeb, h, tax_params, VF_initial,
-     Gamma_initial) = args
+    (r, alpha_k, alpha_l, delta, psi, fixed_cost, betafirm, kgrid, zgrid,
+     bgrid, Pi, eta0, eta1, eta2, s, sizek, sizez, sizeb, h, tax_params,
+     VF_initial, Gamma_initial) = args
     golden_ratio = 2 / (np.sqrt(5) + 1)
 
     # Use the golden ratio to set the initial test points
@@ -145,9 +145,9 @@ def golden_ratio_eqm(lb, ub, args, tolerance=1e-4):
             # Set the new lower test point
             x1 = ub - golden_ratio * (ub - lb)
             # print('New Lower Test Point = ', x1)
-            args = (r, alpha_k, alpha_l, delta, psi, betafirm, kgrid, zgrid,
-                    bgrid, Pi, eta0, eta1, eta2, s, sizek, sizez, sizeb, h,
-                    tax_params, VF1, Gamma1)
+            args = (r, alpha_k, alpha_l, delta, psi, fixed_cost,
+                    betafirm, kgrid, zgrid, bgrid, Pi, eta0, eta1, eta2,
+                    s, sizek, sizez, sizeb, h,  tax_params, VF1, Gamma1)
             f1, VF1, Gamma1 = Market_Clearing(x1, args)
         else:
             # print('f2 < f1')
@@ -169,9 +169,9 @@ def golden_ratio_eqm(lb, ub, args, tolerance=1e-4):
             # Set the new upper test point
             x2 = lb + golden_ratio * (ub - lb)
             # print('New Upper Test Point = ', x2)
-            args = (r, alpha_k, alpha_l, delta, psi, betafirm, kgrid, zgrid,
-                    bgrid, Pi, eta0, eta1, eta2, s, sizek, sizez, sizeb, h,
-                    tax_params, VF2, Gamma2)
+            args = (r, alpha_k, alpha_l, delta, psi, fixed_cost, betafirm,
+                    kgrid, zgrid, bgrid, Pi, eta0, eta1, eta2, s, sizek,
+                    sizez, sizeb, h, tax_params, VF2, Gamma2)
             f2, VF2, Gamma2 = Market_Clearing(x2, args)
 
     # Use the mid-point of the final interval as the estimate of the optimzer
